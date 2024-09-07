74b0301c6951570a105df138be9d2e2f775ec796
Jabcki <willian.gilson@gmail.com>
2024-09-07 18:03:18 -0300

    Aula 3

--- Meu_Primeiro_Proj_Java/Sintaxe Java/src/SintaxeJava.java
+++ Meu_Primeiro_Proj_Java/Sintaxe Java/src/SintaxeJava.java
@@ -1,5 +1,15 @@
 public class SintaxeJava {
+        String meuNome = "William";
+        int anoNascimento = 1991;
+        boolean verFal;
+        public static String nomeCompleto (String primeiroNome, String segundoNome){
+            return "Resultado do metodo " + primeiroNome.concat(" ").concat(segundoNome);
+        }
         public static void main(String[] args) {
-            System.out.print("Heloooooo, How are yo doing?");
+            String primeiroNome = "Wlliam";
+            String segundoNome = "Jonas";
+            String nomeCompleto = nomeCompleto (primeiroNome,segundoNome);
+
+            System.out.println(nomeCompleto);
         }
     }
