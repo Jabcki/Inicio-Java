0e1da5b1b1d80d72451b7e89b67d800a0752ac9b
Jabcki <willian.gilson@gmail.com>
2024-09-16 18:42:32 -0300

    aula 3 operadores

--- Meu_Primeiro_Proj_Java/Sintaxe Java/src/SintaxeJava.java
+++ Meu_Primeiro_Proj_Java/Sintaxe Java/src/SintaxeJava.java
@@ -1,25 +1,12 @@
 public class SintaxeJava {//casse
     public static void main(String[] args){
-        int modulo = 18 % 3;
-        double resultado = (10 * 7) + (20/4);
-        String nomeCompleto = "LINGUAGEM"+"JAVA";
+        int numero = 5;
 
-        String concatenaçao = "?";
-        System.out.println(concatenaçao);
+        numero = - numero;
+        
+        System.out.println(numero);
 
-        concatenaçao = 1+1+1+"1";
-        System.out.println(concatenaçao);
-
-        concatenaçao = 1+"1"+1+1;
-        System.out.println(concatenaçao);
-
-        concatenaçao = 1+"1"+1+"1";
-        System.out.println(concatenaçao);
-
-        concatenaçao = "1"+1+1+1;
-        System.out.println(concatenaçao);
-
-        concatenaçao = "1"+(1+1+1);
-        System.out.println(concatenaçao);
+        numero = numero * -1;
+        System.out.println(numero);
     }
-    }
+}
