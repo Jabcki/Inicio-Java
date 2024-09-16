93c8089e425175e3ed70ad78ed63ff118104b14b
Jabcki <willian.gilson@gmail.com>
2024-09-16 19:03:36 -0300

    Aula 6 Operadores

--- Meu_Primeiro_Proj_Java/Sintaxe Java/src/SintaxeJava.java
+++ Meu_Primeiro_Proj_Java/Sintaxe Java/src/SintaxeJava.java
@@ -1,15 +1,7 @@
 public class SintaxeJava {//casse
     public static void main(String[] args){
-        int a, b;
-        a = 6;
-        b = 6;
-        String resultado = a==b ?"verdadeiro" :"falso";
-        
-        /*if(a==b)
-            resultado = "verdadeiro";
-        else
-            resultado = "falso";
-        */
-            System.out.println(resultado);
+        String nomeUm = "William";
+        String nomeDois = new String("William");
+        System.out.println(nomeUm.equals(nomeDois));
     }
 }
