063b0efdd6dfcb47ac07538ead0dd735ca26bf6b
Jabcki <willian.gilson@gmail.com>
2024-09-22 18:57:06 -0300

    Aula SmarTv final

--- Meu_Primeiro_Proj_Java/Sintaxe Java/src/SmartTv.java
+++ Meu_Primeiro_Proj_Java/Sintaxe Java/src/SmartTv.java
@@ -3,10 +3,28 @@ public class SmartTv {
     int canal = 1;
     int volume = 25;
 
+    public void mudarCanal(int novoCanal){
+        canal = novoCanal;
+    }
+
+    public void aumentarCanal(){
+        canal=++;
+    }
+    public void ddiminuirCanal(){
+        canal=--;
+    }
+
+    public void aumentarVolume(){
+        volume++;
+    }
+    public void diminuirVolume(){
+        volume--;
+    }
+
     public void ligar (){
         ligada = true;
     }
-    public void desligar(){
+    public void desligar (){
         ligada = false;
     }
 }
--- Meu_Primeiro_Proj_Java/Sintaxe Java/src/Usuario.java
+++ Meu_Primeiro_Proj_Java/Sintaxe Java/src/Usuario.java
@@ -1,6 +1,16 @@
 public class Usuario {
 public static void main(String[] args) throws Exception {
     SmartTv smartTv = new SmartTv();
+
+    smartTv.diminuirVolume();
+    smartTv.diminuirVolume();
+    smartTv.diminuirVolume();
+    smartTv.aumentarVolume();
+
+    smartTv.mudarCanal(13);
+    System.out.println("Canal Atual: " + smartTv.canal);
+    
+    System.out.println("Volume Atual: " +smartTv.volume);
     
     System.out.println("TV Ligada?: " + smartTv.ligada);
     System.out.println("Canal Atual: " + smartTv.canal);
