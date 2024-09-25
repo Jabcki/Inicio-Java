bfd4a37048614e02b23f4cd79f4c64b7e19e0d24
Jabcki <willian.gilson@gmail.com>
2024-09-25 20:02:19 -0300

    Aula de documentação

---
+++ Meu_Primeiro_Proj_Java/Sintaxe Java/src/Calculadora.java
@@ -0,0 +1,22 @@
+    /**
+     * <h1>Calculadora</h1>
+     * A Calculadora realiza operações matematicas entre numeros inteiros
+     * <p>
+     * <b>Note:</b> Leia atentamente a documentação desta classe 
+     * para desfrutar dos recursos oferecidos pelo autor
+     * 
+     * @author William Souza
+     * @version 1.0
+     * @since 25/09/2024
+     */
+    public class Calculadora {
+        /**
+         * Este metodo é utilizado para somar dois numeros inteiros
+         * @param numeroUm este é o primeiro parametro do metodo
+         * @param numeroDois este é o segundo parametro do metodo
+         * @return int o resultado deste metodo é a soma de dois numeros.
+         */
+        public int somar(int numeroUm, int numeroDois){
+            return numeroUm + numeroDois;
+        }
+    }
---
+++ Meu_Primeiro_Proj_Java/Sintaxe Java/src/Comentarios.java
@@ -0,0 +1,16 @@
+public class Comentarios {
+    public static void main(String[] args) {
+        // comentario em unica linha
+        /*esse
+         * é um comentario 
+         mais detalhado*/
+
+    }
+    public void metodo(){
+        /**
+         * os dois asteriscos é para 
+         * identificar que voec
+         * pretende elaborar um comentario a nivel de documentação
+         */
+    }
+}
